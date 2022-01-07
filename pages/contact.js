5992fb70b6b248f813e63afc3a3d3c9bd0dd71a4
Joshua Dudley <dudley.joshua789@gmail.com>
2022-01-06 21:56:48 -0600

    contact css fixed

--- pages/contact.js
+++ pages/contact.js
@@ -2,14 +2,14 @@ import {  AiOutlineMail } from 'react-icons/ai'
 
 export default function Contact(){
     return(
-        <div className = "h-max">
+        <div className = "page h-full">
             <h1 className ="text-center text-4xl font-extrabold">Lets Connect!</h1>
-            <div className = "grid grid-cols-2">
-                <div className = "grid justify-items-center pt-24">
+            <div className = "flex">
+                <div className = "flex justify-center items-center w-1/2">
                     <AiOutlineMail size = "300"/>
                 </div>
 
-                <div className = "pt-20 text-black text-xl">             
+                <div className = "w-1/2 pt-20 text-black text-xl">             
                     <input placeholder = "Name" className = "block mb-10 rounded-3xl w-1/2 h-10 px-5" />
                     <input placeholder = "Email" className = "block mb-10 rounded-3xl w-1/2 h-10 px-5"  />
                     <input placeholder = "Subject" className = "block mb-10 rounded-3xl w-1/2 h-10 px-5" />
