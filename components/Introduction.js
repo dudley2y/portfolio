2413ce729f7382883d98e864a750fa3537e68945
Joshua Dudley <dudley.joshua789@gmail.com>
2022-03-07 05:06:25 -0600

    Fixed error

--- components/Introduction.js
+++ components/Introduction.js
@@ -7,13 +7,12 @@ const Introduction = () => {
             <div className = "about-me"> 
                 <p className = "text-xl">Developer:</p>
                 <p className = "text-4xl">Joshua Dudley</p>
-                <div className = "intro-speech">
-                    <p>{   `I am a third year Computer Science student at the <a href= "https://www.uic.edu/"><b>University of Illinois at Chicago </b></a> 
-                        with an interest in developing high performance applications with an emphasis on scalability and security. I've been developing websites
+                <div className = "intro-speech ">
+                    <p>I am a third year Computer Science student at the  <a href= "https://www.uic.edu/"><b>University of Illinois at Chicago </b></a> 
+                    with an interest in developing high performance applications with an emphasis on scalability and security. I've been developing websites
                         since 2018 learning web technologies such as React , Node.js/Express, Next.js. More recently picking up DevOps technologies such as 
                         Docker, Kubernetes, Github Actions to host my applications on Heroku. I'm looking for Summer Software Developer Internships or Fall Co-ops! 
-                        `}
-                        </p>
+                     </p>             
                 </div>
             </div>
             
